5544d5c1daf43a0246327691eb3a47f683d9a7e9
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2010-08-29 11:29:50 +0200

    splitting tokens and calculating positions correctly.

--- nomnom.el
+++ nomnom.el
@@ -11,14 +11,21 @@
        "\\|\{" ;; start curly
        "\\|\}\\)")) ;; end curly
 
-(defun nom/split-tokens (str)
-  (mapcan (lambda (s)
-	    (loop for start = 0 then (1+ pos)
-		  for pos = (string-match "\\([()]\\)" s start)
-		  while pos
-		  append (list (substring s start pos) (match-string 0 s)) into tokens
-		  finally return (append tokens (list (substring s start)))))
-	  (split-string str "[\s,]" t)))
+(defun nom/split-tokens (s idx)
+  "Split composite tokens into subtokens and calculate correct position for each."
+  (remove-if (lambda (token) (string-match "[\s,]" (car token)))
+	     (loop for start = 0 then (1+ pos)
+		   for pos = (string-match "\\([()\s,]\\)" s start)
+		   while pos
+		   append (list 
+			   (list (substring s start pos) 
+				 (+ idx start) (+ idx pos))
+			   (list (match-string 0 s)
+				 (match-beginning 0) (match-end 0))) 
+		   into tokens
+		   finally return (append tokens (list 
+						  (list (substring s start)
+							(+ idx start) (+ idx (length s))))))))
 
 (defun nom/tokenize-buffer ()
   (save-excursion
@@ -29,4 +36,4 @@
       (remove-if-not #'identity
 		     (loop for pos = 0 then (string-match *nom/tokens* *nom/doc* (1+ pos))
 			   while pos
-			   append (nom/split-tokens (match-string 0 *nom/doc*)))))))
+			   append (nom/split-tokens (match-string 0 *nom/doc*) (match-beginning 0)))))))
