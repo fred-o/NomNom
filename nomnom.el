beb682e7dea96532db1fd3d24de0603885e8ad78
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2010-08-30 18:11:38 +0200

    cleaning

--- nomnom.el
+++ nomnom.el
@@ -26,13 +26,13 @@
 
 (defun nom/tokenize-string (s)
   "Split the string S into tokens."
-       (remove-if-not #'identity 
-		      (loop for start = 0 then end
-			    for pos = (string-match *nom/tokens* s start)
-			    while pos
-			    for end = (match-end 0)
-			    append (nom/split-tokens (match-string 0 s) (match-beginning 0)))))
-  
+  (remove-if-not #'identity 
+		 (loop for start = 0 then end
+		       for pos = (string-match *nom/tokens* s start)
+		       while pos
+		       for end = (match-end 0)
+		       append (nom/split-tokens (match-string 0 s) (match-beginning 0)))))
+
 (defun nom/tokenize-buffer ()
   "Tokenize the current java buffer."
   (let ((s (buffer-string)))
@@ -42,7 +42,13 @@
 (defun nom/expect-token (token tokens)
   (cond ((null tokens) nil)
 	((string-equal token (caar tokens)) tokens)
-	(t (nom/expect-token token (cdr tokens)))))
+	(t (nom/expect-token token (rest tokens)))))
+
+(defun nom/expect-curly-open (tokens)
+  (cond ((null tokens) nil)
+	((string-equal "}" (caar tokens)) nil)
+	((string-equal "{" (caar tokens)) tokens)
+	(t (nom/expect-curly-open (rest tokens)))))
 
 (defun nom/expect-curly-close (tokens)
   (cond ((null tokens) nil)
@@ -51,24 +57,33 @@
 	((string-equal "}" (caar tokens)) tokens)
 	(t (nom/expect-curly-close (rest tokens)))))
 
+(defun nom/expect-class-body (tokens)
+  (let* ((co (nom/expect-curly-open tokens))
+	 (cc (nom/expect-curly-close (rest co))))
+    (when cc
+      (cons
+       (list (cadar co) (caddar cc))
+       (rest cc)))))
+
 (defun nom/expect-class-equivalent (tokens)
   (cond ((null tokens) nil)
 	((or (string-equal "class" (caar tokens))
 	     (string-equal "interface" (caar tokens))
 	     (string-equal "enum" (caar tokens)))
-	 (let* ((c1 (nom/expect-token "{" tokens))
-		(c2 (nom/expect-curly-close (rest c1))))
+	 (let ((cb (nom/expect-class-body (rest tokens))))
 	   (cons
-	    (list 'class (caadr tokens)
-		  (cadar c1) (caddar c2))
-	    (nom/expect-class-equivalent (rest c2)))))
-	(t (nom/expect-class-equivalent (rest tokens)))))
+	    (list (intern (caar tokens)) (caadr tokens)
+		  (car cb))
+	    (rest cb))))))
+
+(defun nom/expect-class-equivalents (tokens)
+  (when tokens
+    (let* ((cls (nom/expect-class-equivalent tokens))
+	   (nxt (nom/expect-class-equivalents (rest cls))))
+      (cons (cons (car cls)
+		  (car nxt))
+	    (rest nxt)))))
 
 (defun nom/parse-buffer ()
   "Parse the current java buffer."
-  (nom/expect-class-equivalent (nom/tokenize-buffer)))
-
-;; (defun count-braces (tokens)
-;;   (format "open: %d, closed: %d" 
-;; 	  (length (remove-if-not (lambda (s) (string-equal "{" (car s))) tokens))
-;; 	  (length (remove-if-not (lambda (s) (string-equal "}" (car s))) tokens))))
+  (nom/expect-class-equivalents (nom/tokenize-buffer)))
